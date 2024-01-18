c709d9a401554546ba0aeea404e6c37416a9a73e
kookie2332 <kookie2332@gmail.com>
2024-01-18 14:34:58 +1100

    Moved target_process, syringe and injected_payload out of unsafe scope.

--- injector/src/main.rs
+++ injector/src/main.rs
@@ -1,36 +1,24 @@
-use std::{ffi::OsStr, path::PathBuf, time::Duration};
-
+use std::time::Duration;
 use dll_syringe::{process::OwnedProcess, Syringe};
 
 fn main() {
-    unsafe {
-        /*
-        let tl = tasklist::Tasklist::new();
-
-        for i in tl {
-            if i.get_pname() == "th123.exe" {
-                let m = i.get_pid();
-                let proc = djin::open_process(m).unwrap();
-                let dll = "giuroll.dll";
-                djin::inject_dll(proc, dll, "exeinit".as_bytes()).unwrap();
-                std::thread::sleep(std::time::Duration::from_secs(5));
-            }
+    // Check if th123.exe is running.
+    let target_process = match OwnedProcess::find_first_by_name("th123.exe") {
+        Some(x) => x,
+        None => {
+            println!("th123.exe process not found, make sure soku is running");
+            std::thread::sleep(Duration::from_secs(5));
+            panic!()
         }
-        */
+    };
 
-        let target_process = match OwnedProcess::find_first_by_name("th123.exe") {
-            Some(x) => x,
-            None => {
-                println!("th123.exe process not found, make sure soku is running");
-                std::thread::sleep(Duration::from_secs(5));
-                panic!()
-            }
-        };
-        let syringe = Syringe::for_process(target_process);
-        let injected_payload = syringe.inject("giuroll.dll").unwrap();
+    // Inject giuroll.dll into the game.
+    let syringe = Syringe::for_process(target_process);
+    let injected_payload = syringe.inject("giuroll.dll").unwrap();
 
+    unsafe {
         match syringe
-            .get_raw_procedure::<unsafe extern "C" fn() -> bool>(
+            .get_raw_procedure::</*unsafe*/ extern "C" fn() -> bool>(
                 injected_payload,
                 "better_exe_init",
             )
